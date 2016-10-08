0865c0f71071c6e108fa539b621e42b57b77126d
DavidMa266 <dsm266@cornell.edu>
2016-10-07 23:44:58 -0400

    This is the plain hex vvariant

--- Ships/Ships/hex.cpp
+++ Ships/Ships/hex.cpp
@@ -1,74 +1,27 @@
 #include "hex.h"
 
 
-Shape*  Shape::get_parent()
-{
-	return Shape::parent;
-}
-std::vector<Shape*> Shape::get_children()
-{
-	return Shape::children;
-}
-int Shape::get_children_num()
-{
-	return Shape::children_num;
-}
-Ship* Shape::get_ship()
+Ship* Hex::get_ship()
 {
 	return Shape::ship;
 }
-bool Shape::get_has_ship()
+bool Hex::get_has_ship()
 {
 	return Shape::has_ship;
 }
-double Shape::get_x()
+int Hex::get_x()
 {
 	return Shape::x;
 }
-double Shape::get_y()
+int Hex::get_y()
 {
 	return Shape::y;
 }
-double Shape::get_rotation()
-{
-	return Shape::rotation;
-}
 
-//Generic Shape constructor. Initializes all starting values.
-Shape::Shape(Shape* parent, double x, double y, double rotation)
+Hex::Hex(int x, int y)
 {
-	Shape::parent = parent;
-	Shape::x = x;
-	Shape::y = y;
-	Shape::rotation = rotation;
-	Shape::has_ship = false;
-	Shape::ship = nullptr;
-	Shape::children_num = 0;
-	Shape::children;
-}
-
-//Currently nothing is being done to Trapezoids, but I expect I may need to add 'neighbor' nodes and such.
-//That and the image projected will be the main difference between Trapezoids and Hexagonal nodes.
-//This gives me freedom to choose between pinpoint targetting and 'hit everything in a range' for firing.
-Trapezoid::Trapezoid(Shape * parent, double x, double y, double rotation) : Shape(parent, x, y, rotation)
-{
-}
-Hex::Hex(bool size_small, Shape* parent, double x, double y, double rotation) : Shape(parent, x, y, rotation)
-{
-	if (size_small) 
-	{
-	}
-	else 
-	{
-		Hex::children_num = 7;
-		std::vector<Shape*> children;
-		Hex* h_child = new Hex(true, this, x, y, rotation);
-		children.push_back(h_child);
-		
-		for (int i = 0; i < 6; i++) {
-			Trapezoid* t_child = new Trapezoid(this, x, y, rotation);
-			children.push_back(t_child);
-		}
-		Hex::children = children;
-	}
+	Hex::x = x;
+	Hex::y = y;
+	Hex::has_ship = false;
+	Hex::ship = nullptr;
 }
--- Ships/Ships/hex.h
+++ Ships/Ships/hex.h
@@ -5,40 +5,19 @@
 #include <vector>
 #include "ship.h"
 
-//Shapes represent the individual components that make up the entire game board. 
-//I will probably have to add a Image of some parameter associated with a shape.
-class Shape 
-{
+class Hex {
 protected:
-	Shape* parent;
-	std::vector<Shape*> children;
-	int children_num;
 	Ship* ship;
 	bool has_ship;
-	double x, y, rotation;
+	int x, y;
 
 public:
-	Shape(Shape* parent, double x, double y, double rotation);
-	Shape* get_parent();
-	std::vector<Shape*> get_children();
-	int get_children_num();
+	Hex(int x, int y);
 	Ship* get_ship();
 	bool get_has_ship();
-	double get_x();
-	double get_y();
-	double get_rotation();
+	int get_x();
+	int get_y();
 };
 
-class Trapezoid : public Shape
-{
-public:
-	Trapezoid(Shape* parent, double x, double y, double rotation);
-};
-
-class Hex : public Shape
-{
-public:
-	Hex(bool size_small, Shape* parent, double x, double y, double rotation);
-};
 
 #endif
